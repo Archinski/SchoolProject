3f4386de1f04aa8a5d4936b13a6a89e12116a75a
asdtt <asdttooppp@gmail.com>
2024-11-07 20:18:58 +0500

    Homework migration, index

--- school/src/main/resources/liquibase/scripts/index-practice.sql
+++ school/src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,7 +1,7 @@
--- liquibase formatted sql
+ -- liquibase formatted sql
 
--- changeset asdt:1
-CREATE INDEX index_student_name ON student(name);
+ -- changeset asdt:1
+ CREATE INDEX index_student_name ON student(name);
 
--- changeset asdt:2
-CREATE INDEX index_faculty_name_color ON faculty(name, color);
+ -- changeset asdt:2
+ CREATE INDEX index_faculty_name_color ON faculty(name, color);
